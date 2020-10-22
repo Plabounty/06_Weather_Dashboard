9b9284bafbafb127d9f22001abebf5be349cb0d2
Jake Fireside <labountypaul@icloud.com>
2020-10-21 20:42:30 -0500

    Added API rendering on the page from the Weather API to show up on HTML

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var APIKey = 'e7610467436ab1c59773adeceb236ff7'
 
     function weatherCall(city) {
         console.log(city)
-    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=' + APIKey;
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=' + APIKey + '&units=imperial';
 
         $.ajax({
             url: queryURL,
@@ -12,28 +12,34 @@ var APIKey = 'e7610467436ab1c59773adeceb236ff7'
             .then(function(response) {
                 console.log(queryURL)
                 console.log(response) 
+                $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+                $('.temp').text('Temperature: (F)' + response.main.temp)
+                $('.humidity').text('Humidity:' + response.main.humidity)
+                $('.windSpeed').text('Wind Speed:' + response.wind.speed)
+                console.log("Humidity: " + response.main.humidity)
+                
             });
         }
 
-        function fiveDayCall(city) {
-            console.log(city)
-        var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=' + APIKey;
+        // function fiveDayCall(city) {
+        //     console.log(city)
+        // var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=' + APIKey;
     
-            $.ajax({
-                url: queryURL,
-                method: 'GET'
-                })
+        //     $.ajax({
+        //         url: queryURL,
+        //         method: 'GET'
+        //         })
     
-                .then(function(response) {
-                    console.log(queryURL)
-                    console.log(response) 
-                });
-            }
+        //         .then(function(response) {
+        //             console.log(queryURL)
+        //             console.log(response) 
+        //         });
+        //     }
 
     
     $('#btn').on('click', function(){
        var search = $('#location').val()
        weatherCall(search)
-       fiveDayCall(search)
+    //    fiveDayCall(search)
        
     })
